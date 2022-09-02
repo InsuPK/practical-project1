924987922eddd1bc060f3331923a3f77a66048fd
InsuPK <107782364+InsuPK@users.noreply.github.com>
2022-09-02 10:22:39 +0900

    Apply .gitingore

---
+++ src/main/java/com/mycom/word/Word.java
@@ -0,0 +1,33 @@
+package com.mycom.word;
+
+public class Word {
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+    public Word() {
+
+    }
+
+    public Word(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
