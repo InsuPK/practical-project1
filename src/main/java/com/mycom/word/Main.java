7f71bb9d7f133aa2f7a891d45341e7dcd3eb0526
InsuPK <107782364+InsuPK@users.noreply.github.com>
2022-09-06 00:43:19 +0900

    update 9/6

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -1,6 +1,7 @@
+package com.mycom.word;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.exit(0);
+        new WordManager().start();
     }
 }
--- src/main/java/com/mycom/word/Word.java
+++ src/main/java/com/mycom/word/Word.java
@@ -7,12 +7,29 @@ public class Word {
     private String meaning;
 
     public Word() {
-
     }
 
-    public Word(String word, String meaning) {
+    public Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
         this.word = word;
         this.meaning = meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 
     public String getWord() {
@@ -30,4 +47,16 @@ public class Word {
     public void setMeaning(String meaning) {
         this.meaning = meaning;
     }
+
+    @Override
+    public String toString() {
+        String slevel = "";
+        for(int i = 0; i < level; i++) {
+            slevel += "*";
+        }
+        String str = String.format("%-3s", slevel) +
+                String.format("%15s", word) + "  " + meaning;
+        return str;
+    }
+
 }
