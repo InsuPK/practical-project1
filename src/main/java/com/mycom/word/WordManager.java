099d90bfd4e53088a9b753d2ba0f0ca770d49151
InsuPK <107782364+InsuPK@users.noreply.github.com>
2022-09-17 17:43:21 +0900

    delete option

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -33,13 +33,13 @@ public class WordManager {
                 break;
             }
             if (menu == 4) {
-                wordCRUD.addWord();
+                wordCRUD.addItem();
             } else if (menu == 1) {
                 wordCRUD.listAll();
             } else if (menu == 5) {
                 wordCRUD.updateItem();
             } else if (menu == 6) {
-                wordCRUD.listAll();
+                wordCRUD.deleteItem();
             }
 
         }
